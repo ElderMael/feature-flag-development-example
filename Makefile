7ab4131aa2a2de7d1ab7ecd094043854f42c2f91
ElderMael <sftwr.mael@gmail.com>
2024-08-16 00:10:17 -0600

    Create CLI And Front End Components

---
+++ Makefile
@@ -0,0 +1,24 @@
+.PHONY: build-frontend
+build-frontend:
+	cd frontend; docker build -t eldermael:frontend .
+
+.PHONY: build-api
+build-api:
+	cd api; docker build -t eldermael:api .
+
+.PHONY: build-cli
+build-cli:
+	cd cli; cargo build
+
+.PHONY: build
+build: build-api build-cli build-frontend
+	echo "Building all components"
+
+.PHONY: deploy
+deploy:
+	helmfile apply
+	cp cli/target/debug/ff "${HOME}/.local/bin"
+
+.PHONY: build-deploy
+build-deploy: build deploy
+	echo "Building all components and then deploying"
--- api/src/main/java/io/eldermael/ff/example/controller/FeatureFlagController.java
+++ api/src/main/java/io/eldermael/ff/example/controller/FeatureFlagController.java
@@ -3,15 +3,13 @@ package io.eldermael.ff.example.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 public class FeatureFlagController {
 
     @GetMapping("/")
-    public String index() {
-        return """
-                {
-                    "success": "ok"
-                }
-                """;
+    public Map<String, Object> index() {
+        return Map.of("success", true);
     }
 }
